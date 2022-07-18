0bac309fb3cb77a4e470b7dd65ed6ce4454d3456
unknown <sarasaravevic@gmail.com>
2022-07-18 21:13:52 +0200

    polovicno

---
+++ app.js
@@ -0,0 +1,35 @@
+let inputs = document.querySelectorAll("input");
+let errors = {
+  ime_prezime: [],
+  korisnicko_ime: [],
+  email: [],
+  lozinka: [],
+  ponovi_lozinku: [],
+};
+inputs.forEach((element) => {
+  element.addEventListener("change", (e) => {
+    let currentInput = e.target;
+    let inputValue = currentInput.value;
+    let inputName = currentInput.getAttribute("name");
+    if (inputValue.length > 4) {
+      console.log("idealno");
+    } else {
+      errors[inputName] = ["polje mora imati najmanje 5 karaktera"];
+    }
+    //document.querySelector("div").innerHTML = `<ul><li>${errors[inputName][0]}</li></ul>`;
+  });
+});
+const populateErrors = (errors) => {
+  for (let key of Object.keys(errors)) {
+    let input = document.querySelector(`input[name="${key}"]`);
+    let parentElement = input.parentElement;
+    let errorsElement = document.createElement("ul");
+    parentElement.appendChild(errorsElement);
+
+    errors[key].forEach((error) => {
+      let li = document.createElement("li");
+      li.innerHTML = error;
+      errorsElement.appendChild(li);
+    });
+  }
+};
